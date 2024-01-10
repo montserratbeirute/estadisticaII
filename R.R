f8e04b2b0f46b355a4176008830f1988b0452c2d
montserratbeirute <143130080+montserratbeirute@users.noreply.github.com>
2024-01-10 16:29:30 -0600

    creacion

---
+++ R.R
@@ -0,0 +1,11 @@
+#---CSV y formatos--------------------------------------------------------
+
+# Abrir y dar formato a la base de datos
+
+# Base de datos de alcohol
+
+base_salarios <- read.csv("BaseSalarios.csv",  sep = ";")
+
+#
+
+#-------------------------------------------------------
